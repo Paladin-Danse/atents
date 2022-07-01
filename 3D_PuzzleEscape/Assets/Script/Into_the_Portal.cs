96cd362a0c02ce70d8e7bc36a27e3139053c7c8c
hshhwa <37408775+Paladin-Danse@users.noreply.github.com>
2022-07-01 15:55:38 +0900

    3D 퍼즐 방탈출
    * 오브젝트 구현
    - 포탈(임시) 완성.
    
    * 해야될 일
    - 포탈과 상호작용하여 게임의 엔딩 구현.
    - 아무것도 장비하지 않은 상태임에도 세면대와 상호작용하면 마지막에 사용한 마네킹의 부품을 얻는 버그가 있음.(아마도 인벤토리에는 사라졌지만 Selected Item은 null이 되지 않았기 때문일 듯.)

---
+++ 3D_PuzzleEscape/Assets/Script/Into_the_Portal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Into_the_Portal : InteractionObject
+{
+    [SerializeField] private GameObject GameEndingUI;
+
+    // Start is called before the first frame update
+    protected new void Start()
+    {
+        base.Start();
+        GameEndingUI.SetActive(false);
+    }
+
+    public void Ending()
+    {
+        GameEndingUI.SetActive(true);
+    }
+}
