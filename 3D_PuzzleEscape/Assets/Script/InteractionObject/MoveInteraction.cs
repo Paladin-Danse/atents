1cb6084998e85d928a617968abee3cfa19a5a265
hshhwa <37408775+Paladin-Danse@users.noreply.github.com>
2022-07-08 16:11:41 +0900

    3D 퍼즐 방탈출
    * 기능 구현.
    - 움직이는 상호작용 물체가 움직일 때 소리가 나는 코드 작성
    
    * 해야 될 일.
    - 오디오 소스도 오디오 리스너도 있음에도 소리가 안 들림. 뭐가 문제인지 원인 파악이 필요.
    - 세이브 문제도 아직 남아 있음.

--- 3D_PuzzleEscape/Assets/Script/InteractionObject/MoveInteraction.cs
+++ 3D_PuzzleEscape/Assets/Script/InteractionObject/MoveInteraction.cs
@@ -10,9 +10,15 @@ public class MoveInteraction : InteractionObject
     protected bool b_OnMove;
     [SerializeField] protected float f_MoveTime;
 
+    [SerializeField] protected bool Audio_Add_Clip_Chk;
+    [SerializeField] protected AudioClip primary_Clip;
+    [SerializeField] protected AudioClip additional_Clip;
+    AudioSource Interaction_Sound;
+
     private void Awake()
     {
         MoveAnimation = GetComponent<Animation>();
+        Interaction_Sound = GetComponent<AudioSource>();
     }
 
     protected new void Start()
@@ -21,6 +27,9 @@ public class MoveInteraction : InteractionObject
 
         if (!MoveAnimation) Debug.Log("MoveAnimation is Not Found!!");
         else SetAnim(MoveClip);
+
+        if (!primary_Clip) Debug.LogError(gameObject.ToString() + "Not Found primary_Clip!");
+        if (Audio_Add_Clip_Chk && !additional_Clip) Debug.LogError(gameObject.ToString() + "Not Found additional_Clip : please Uncheck 'Audio_Add_Source_Chk' or enter the Clip.");
 
         e_ObjectType = OBJ_TYPE.OBJ_INTERACT;
         InteractionEvent += InteractiontoMove;
@@ -33,14 +42,18 @@ public class MoveInteraction : InteractionObject
         if (b_OnMove)
         {
             MoveAnimation[MoveAnimation.clip.name].speed = -1.0f;
+            if(Audio_Add_Clip_Chk)Interaction_Sound.clip = additional_Clip;
+            else Interaction_Sound.clip = primary_Clip;
             b_OnMove = false;
         }
         else
         {
             MoveAnimation[MoveAnimation.clip.name].speed = 1.0f;
+            Interaction_Sound.clip = primary_Clip;
             b_OnMove = true;
         }
         MoveAnimation.Play();
+        if (Interaction_Sound.clip) Interaction_Sound.Play();
     }
 
     public void MovePause()
