e41b88a528aab90d3f57ea1f58af2505cc3ee135
hshhwa <37408775+Paladin-Danse@users.noreply.github.com>
2022-07-14 16:06:31 +0900

    3D 퍼즐 방탈출
    * 기능 구현
    - 오디오 옵션 100% 완료
    - 세이브파일에 저장될 값 목록 파일을 생성.(해당 파일은 완성본이 아님. 이후 목록이 추가 될 수 있음.)
    
    * 해야 될 일
    - 세이브파일 목록 생각나는대로 추가.
    - 세이브데이터를 저장할 클래스를 추가하여 상술한 목록의 데이터를 해당 클래스에 담기

--- 3D_PuzzleEscape/Assets/Script/Manager/AudioManager.cs
+++ 3D_PuzzleEscape/Assets/Script/Manager/AudioManager.cs
@@ -19,33 +19,45 @@ public class AudioManager : MonoBehaviour
     }
 
     [SerializeField] private AudioMixer MasterMixer;
+    private AudioSource BGM;
+    private float maxVolume;
+    private float minVolume;
+    private int SliderValue;
     private int Volume;
 
     private void Awake()
     {
+        
         //원래 세이브데이터에서 가져와야 할 값을 상수값으로 대체하였다. 이후 세이브기능이 구현되면 다시 손볼 것!
-        Volume = 100;
+        Volume = 50;
+        BGM = GetComponent<AudioSource>();
     }
     private void Start()
     {
+        maxVolume = UIManager.instance.Slider_Audio.maxValue;
+        minVolume = UIManager.instance.Slider_Audio.minValue;
+
+        SliderValue = (int)-((maxVolume - minVolume) - (maxVolume - minVolume) * Volume / 100);
+        MasterMixer.SetFloat("Master", SliderValue);
+        UIManager.instance.Slider_Audio.value = SliderValue;
+        BGM.Play();
         DontDestroyOnLoad(gameObject);
     }
     //볼륨의 슬라이더를 움직이면 불려오는 함수
     public void AudioControl()
     {
-        float sound;
         if (UIManager.instance.Slider_Audio)
         {
-            sound = UIManager.instance.Slider_Audio.value;
-            Volume = (int)((sound - UIManager.instance.Slider_Audio.minValue) / (UIManager.instance.Slider_Audio.maxValue - UIManager.instance.Slider_Audio.minValue) * 100.0f);
+            SliderValue = (int)UIManager.instance.Slider_Audio.value;
+            Volume = (int)((SliderValue - minVolume) / (maxVolume - minVolume) * 100.0f);
         }
         else
         {
             Debug.Log("Wrong Acess : Not Found AudioSlider");
             return;
         }
-        if (sound <= -40f) MasterMixer.SetFloat("Master", -80f);
-        else MasterMixer.SetFloat("Master", sound);
+        if (SliderValue <= -40f) MasterMixer.SetFloat("Master", -80f);
+        else MasterMixer.SetFloat("Master", SliderValue);
 
         UIManager.instance.SetVolumeText(Volume.ToString());
     }
